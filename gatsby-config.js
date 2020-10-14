0d9a6c753e35b5ff5eb4ac29fefc3f21702acd90
Hersey <hersey@Herseys-iMac.local>
2020-10-14 19:08:37 +0100

    Add pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby with Drupal Testing`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,70 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+
+    // CURRENTLY CREATING 2 HOME PAGES ON BOTH PATHS!!!!
+    // NEED TO SOLVE THE PATH ISSUE OUT AND ENSURE WE CREATE EVERYTHING ON APPROPRIATE PATH
+    // SEE BELOW - NOT IDEAL BUT AT THIS STAGE WORKING
+    // CREATE HOME (CURRENTLY ARTICLES)
+    // const index = await graphql(`
+    //     {
+    //         allNodePage {
+    //             edges {
+    //                 node {
+    //                     id
+    //                 }
+    //             }
+    //         }
+    //     }
+    // `)
+
+    // const indexTemplate = path.resolve("src/templates/index.js")
+
+    // index.data.allNodePage.edges.forEach(edge => {
+    //     createPage({
+    //         path: `/`,
+    //         component: indexTemplate,
+    //         context: {
+    //             id: edge.node.id,
+    //         },
+    //     })
+    // })
+    // CREATE GENERIC PAGE QUERY END
+    
+    // CREATE PAGES
+    const page = await graphql(`
+        {
+            allNodePage {
+                edges {
+                    node {
+                        id
+                        path {
+                            alias
+                        }
+                    }
+                }
+            }
+        }
+    `)
+
+    const pageTemplate = path.resolve("src/templates/page.js")
+        page.data.allNodePage.edges.forEach(edge => {
+            console.log('ALIAS?', edge.node.path.alias)
+            // NOT THE RIGHT WAY, PATH SHOULD BE BUILT BY SOMETHING LIKE THE ID/SLUG
+            // BUT WHILE STILL PLAYI9NG THIS ALLOWS A SINGLE TPL QUERY STRUCTURE
+            const pageAlias = edge.node.path.alias ? edge.node.path.alias : "/"
+            console.log('PAGE ALIAS:', pageAlias)
+            createPage({
+                path: pageAlias,
+                component: pageTemplate,
+                context: {
+                    id: edge.node.id,
+                },
+            })
+        })
+        // CREATE PAGES END
+  }
